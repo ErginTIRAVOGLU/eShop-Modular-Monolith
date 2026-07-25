b00ab2159aad0807f770c49a7bd6fb304849684a
Ergin <ergintiravoglu@msn.com>
2026-07-25 11:53:07 +0300

    Added Pagination

--- src/Modules/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
+++ src/Modules/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
@@ -1,14 +1,17 @@
+using Shared.Pagination;
+
 namespace Catalog.Products.Features.GetProducts;
 
-public record GetProductsResponse(IEnumerable<ProductDto> Products);
+public record GetProductsRequest(PaginationRequest PaginationRequest);
+public record GetProductsResponse(PaginatedResult<ProductDto> Products);
 
 public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
+        app.MapGet("/products", async ([AsParameters] PaginationRequest request, ISender sender) =>
         {
-            var query = new GetProductsQuery();
+            var query = new GetProductsQuery(request);
             var result = await sender.Send(query);
             var response = result.Adapt<GetProductsResponse>();
             return Results.Ok(response);
